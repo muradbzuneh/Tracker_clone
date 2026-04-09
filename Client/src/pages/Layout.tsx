0a4bb94af943fa0c81b8491bdeeaefad392a2d87
Mbeth <muradbzuneh@gmail.com>
2026-04-09 12:17:55 -0700

    change the fake path to the correct path for layout

--- Client/src/pages/Layout.tsx
+++ Client/src/pages/Layout.tsx
@@ -2,24 +2,14 @@ import { Outlet } from "react-router-dom";
 import BottomNav from "../assets/ui/ButtomNav";
 import Sidebar from "./Sidebar";
 
-const Layout = () => {
-  return (
-    <div className="flex h-screen bg-[#07121E] text-white">
-      
-      {/* Sidebar (desktop only) */}
-      <div className="hidden md:block">
-        <Sidebar />
-      </div>
-
-      {/* Main Content */}
-      <div className="flex-1 overflow-y-auto pb-16 md:pb-0">
-        <Outlet />
-      </div>
-
-      {/* Bottom Nav (mobile only) */}
-      <BottomNav />
-    </div>
-  );
-};
+const Layout = () => (
+  <div className="layout-container">
+    <Sidebar />
+    <main className="main-content">
+      <Outlet />
+    </main>
+    <BottomNav />
+  </div>
+);
 
 export default Layout;
